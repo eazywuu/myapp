d3a6f6aef2175cd74df0be094b78ba7f1b73a227
wyz <2947680284@qq.com>
2022-05-25 13:16:20 +0800

    redis success

--- user/src/main/java/xyz/eazywu/controller/IRedisController.java
+++ user/src/main/java/xyz/eazywu/controller/IRedisController.java
@@ -4,18 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xyz.eazywu.common.R;
 import xyz.eazywu.service.IRedisService;
 
 import javax.annotation.Resource;
 
-@Controller
+@RestController
 @RequestMapping("/app/rank")
 public class IRedisController {
     @Resource
     private IRedisService redisService;
 
-    @GetMapping("/getValue/{key}")
-    public String getValue(@PathVariable String key) {
-        return redisService.getValue(key);
+    @GetMapping("/get/{key}")
+    public R getValue(@PathVariable("key") String key) {
+        return R.ok().put("value", redisService.getValue(key));
     }
 }
---
+++ user/src/test/java/xyz/eazywu/RedisTest.java
@@ -0,0 +1,31 @@
+package xyz.eazywu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import xyz.eazywu.service.IRedisService;
+import xyz.eazywu.service.UserService;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisTest {
+    @Resource
+    private IRedisService redisService;
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void test1() {
+        System.out.println(redisService.getValue("key1"));
+    }
+
+    @Test
+    public void test2() {
+        userService.getTotal(1L);
+    }
+}
